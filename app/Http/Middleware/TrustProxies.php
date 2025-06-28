41b825bc10c32ca991edf7fa8d8bd98e303585f7
Ryan Jiro <92157016+ryanjiroo@users.noreply.github.com>
2025-06-28 22:05:12 +0700

    Add files via upload

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Paling umum untuk lingkungan cloud/proxy seperti Vercel
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_FOR |
+                         Request::HEADER_X_FORWARDED_HOST |
+                         Request::HEADER_X_FORWARDED_PORT |
+                         Request::HEADER_X_FORWARDED_PROTO |
+                         Request::HEADER_X_FORWARDED_AWS_ELB; // Tambahkan ini jika Anda menggunakan AWS ELB atau layanan serupa (Vercel mungkin menggunakannya secara internal)
+}
