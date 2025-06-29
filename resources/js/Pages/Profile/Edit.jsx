a2f7a1a4f469c5e6dec0318c9f902efb8f8dbcc0
Ryan Jiro <92157016+ryanjiroo@users.noreply.github.com>
2025-06-29 15:53:23 +0700

    Update Edit.jsx

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,13 +1,17 @@
 import { Head } from '@inertiajs/react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 
-export default function Edit({ mustVerifyEmail, status }) {
+export default function Edit({ mustVerifyEmail, status, auth }) {
     return (
         <>
             <Head title="Profile" />
-
+            <div classname="bg-white">
+                <Navbar auth={auth}/>
+            </div>
             <div className="py-12">
                 <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
@@ -27,6 +31,7 @@ export default function Edit({ mustVerifyEmail, status }) {
                     </div>
                 </div>
             </div>
+            <Footer/>
         </>
 
     );
